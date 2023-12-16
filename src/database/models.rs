b97ad33179fea67a3cfb81bc872b0c367b6b0008
Fidele K <itfidele@gmail.com>
2023-12-17 01:15:33 +0300

    Add database module and models

---
+++ src/database/models.rs
@@ -0,0 +1,18 @@
+use crate::schema::users;
+use diesel::{AsChangeset, Insertable, Queryable, Selectable};
+
+#[derive(Insertable)]
+#[table_name = "users"]
+pub struct NewUser<'a> {
+    pub username: &'a str,
+    pub password: &'a str,
+    pub email: &'a str,
+}
+
+#[derive(Debug,Queryable,Selectable, AsChangeset,)]
+pub struct User {
+    pub id: i32,
+    pub username: String,
+    pub password: String,
+    pub email: String,
+}
